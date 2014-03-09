79ba43ecd26f56cd904bfd857f988ee9735d1b10
ViaPro <zjuwmh@163.com>
2014-03-09 13:27:15 +0800

    Add hanoi

--- recursion.pl
+++ recursion.pl
@@ -40,3 +40,22 @@ sub dir_walk {
 
 # print(total_size("C:/Dev/Proj/recursion"));
 dir_walk("C:/Dev/Proj/recursion", sub {print $_[0], "\n";})
+
+
+# hanoi(N, start, end, extra)
+# Solve Tower of Hanoi problem for a tower of N disks,
+# of which the largest is disk #N. Move the entire tower from
+# peg 'start' to peg 'end', using peg 'extra' as a work space
+
+sub hanoi{
+	my ($n, $start, $end, $extra) = @_;
+	if($n==1){
+		print "Move disk #1 from $start to $end. \n";
+	} else {
+		hanoi($n-1, $start, $extra, $end);
+		print "Move disk #$n from $start to $end. \n";		
+		hanoi($n-1, $extra, $end, $start);
+	}
+}
+
+hanoi(3, 'A', 'C', 'B');
